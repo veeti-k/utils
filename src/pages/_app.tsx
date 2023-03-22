d73a0562299e18c0cf6db7196bc30c58044f1d48
Veeti K <veeti@veetik.com>
2023-03-22 16:27:38 +0200

    ???

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
 import type { AppType } from "next/dist/shared/lib/utils";
 
-import { SalaryContextProvider } from "../SalaryStuff/SalaryContext/SalaryContextProvider";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-	return (
-		<SalaryContextProvider>
-			<Component {...pageProps} />
-		</SalaryContextProvider>
-	);
+	return <Component {...pageProps} />;
 };
 
 export default MyApp;
